63bc5f1f09fa9db705a2b4c0eaf63a1fc9f0066c
HuangYunXuan <hyx0329@163.com>
2022-08-22 14:03:06 +0800

    refactor: use let..in style for future expansion

--- flake.nix
+++ flake.nix
@@ -3,62 +3,56 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     nixos-hardware.url = github:NixOS/nixos-hardware/master;
     nur.url = "github:nix-community/NUR";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    comma.url = "github:nix-community/comma";
+    comma.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {
     self,
     nixpkgs,
-    nixpkgs-unstable,
     nixos-hardware,
     nur,
     home-manager,
     ...
-  } @ attrs: {
-
+  } @ inputs: 
+  # currently I only have one laptop so no "let...in" required
+  # but added for future convenience
+  let
+    overlayModule = {
+      nixpkgs.overlays = [
+        (nur.overlay)
+        #(import ./overlays)
+      ]
+    }
+    userName = "hyx"
+  in
+  {
     nixosConfigurations.tp480 = nixpkgs.lib.nixosSystem {
-      specialArgs = attrs;
+      specialArgs = inputs;
       system = "x86_64-linux";
       modules = [
-        {
-          nixpkgs.overlays = [
-            nur.overlay
-            #(import ./overlays)
-          ];
-        }
-
-        # hardware configs
-        ./hosts/tp480  # customized platform config for hardware
-        ./system/release-configs.nix
-	./system/users/hyx.nix  # my account config, don't forget to set passwd
-        ./system/desktop/gnome.nix  # gnome4 looks cool
-        ./system/locale.nix  # IM, font, etc.
-        ./system/network.nix
-        ./system/services
-        ./system/softwares  # include nix configs
+        overlayModule                 # global overlay module
+        ./hosts/tp480                 # customized platform config for hardware
+        ./system/release-configs.nix  # allow non-free, pin stateVersion
+        ./system/users/hyx.nix        # my account config, don't forget to set passwd
+        ./system/desktop/gnome.nix    # gnome4 looks cool
+        ./system/locale.nix           # IM, font, etc.
+        ./system/network.nix          # network manager and default configs
+        ./system/services             # power, bluetooth, audio, printing
+        ./system/softwares            # include nix configs
 
         # home-manager integration
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          # Make sure the user names are correct
-          # home-manager.users.username = import ./homemanager/home-username.nix;
-          home-manager.users.hyx = import ./homemanager/hyx;
+          home-manager.users.${userName} = import ./homemanager/${userName};
         }
       ];
     };
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
   };
 }
