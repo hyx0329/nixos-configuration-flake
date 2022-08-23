ab3f24b546eda99702db61788131dd4afd5687ec
HuangYunXuan <hyx0329@163.com>
2022-08-23 21:45:48 +0800

    use manual vim config

--- home/hyx/vim.nix
+++ home/hyx/vim.nix
@@ -2,28 +2,38 @@
 
 {
   # This will conflict with homemanager's config below
-  #home.packages = [
-  #  pkgs.vim
-  #];
+  # home manager's vim configuration tool sucks anyway
+  home.packages = [
+    pkgs.vim
+  ];
   
   home.sessionVariables = {
     EDITOR = "vim";
   };
 
-  programs.vim = {
-    enable = true;
-    settings = {
-      expandtab = true;
-      tabstop = 2;
-      number = true;
-    };
-    extraConfig = ''
-      let g:vim_markdown_folding_disabled = 1
-    '';
-    plugins = with pkgs.vimPlugins; [
-      vim-plug
-      YouCompleteMe
-      vim-markdown
-    ];
+  # seems broken, not working, disabled
+  #programs.vim = {
+  #  enable = true;
+  #  settings = {
+  #    expandtab = true;
+  #    tabstop = 2;
+  #    number = true;
+  #  };
+  #  extraConfig = ''
+  #    let g:vim_markdown_folding_disabled = 1
+  #  '';
+  #  plugins = with pkgs.vimPlugins; [
+  #    vim-plug
+  #    YouCompleteMe
+  #    vim-markdown
+  #  ];
+  #};
+
+  # manual configuation
+  home.file.".vimrc".source = ./vim/vimrc;
+  home.file.".vim/autoload/plug.vim".text = builtins.fetchurl {
+    # pure, but may not be the latest
+    url = "https://github.com/junegunn/vim-plug/raw/d94d234548a8fd6fa686812848f377f1419dafa1/plug.vim";
+    sha256 = "e2dbd7c8d732ae797e5059c0dc1e964b94529a32d07d051d5d058727462a43fd";
   };
 }
