362a8a16f6baea2f82efab520b59639ed518e614
HuangYunXuan <hyx0329@163.com>
2022-08-23 11:42:55 +0800

    add footterm config

--- homemanager/hyx/terminal.nix
+++ homemanager/hyx/terminal.nix
@@ -34,4 +34,55 @@
       theme = "fishy";
     };
   };
+
+  # foot terminal config
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        term = "xterm-256color";
+        dpi-aware = "yes";
+      };
+      mouse = {
+        hide-when-typing = "yes";
+      };
+      colors = {
+        # breeze color scheme
+        # alpha=1.0
+        foreground = "fcfcfc";
+        background = "232627";
+        
+        ## Normal/regular colors (color palette 0-7)
+        regular0 = "232627";  # black
+        regular1 = "ed1515";  # red
+        regular2 = "11d116";  # green
+        regular3 = "f67400";  # yellow
+        regular4 = "1d99f3";  # blue
+        regular5 = "9b56b6";  # magenta
+        regular6 = "1abc9c";  # cyan
+        regular7 = "fcfcfc";  # white
+        
+        ## Bright colors (color palette 8-15)
+        bright0 = "7f8c8d";   # bright black
+        bright1 = "c0392b";   # bright red
+        bright2 = "1cdc9a";   # bright green
+        bright3 = "fdbc4b";   # bright yellow
+        bright4 = "3daee9";   # bright blue
+        bright5 = "8e44ad";   # bright magenta
+        bright6 = "16a085";   # bright cyan
+        bright7 = "ffffff";   # bright white
+        
+        ## dimmed colors (see foot.ini(5) man page)
+        dim0 = "31363b";   # dim black
+        dim1 = "783228";   # dim red
+        dim2 = "17a262";   # dim green
+        dim3 = "b65619";   # dim yellow
+        dim4 = "1b668f";   # dim blue
+        dim5 = "614a73";   # dim magenta
+        dim6 = "186c60";   # dim cyan
+        dim7 = "63686d";   # dim white
+      };
+    };
+  };
+  
 }
