f626e27ac8601ae2faed56d520a0a514053c7385
HuangYunXuan <hyx0329@163.com>
2022-08-25 16:31:38 +0800

    add telegram

--- home/hyx/softwares.nix
+++ home/hyx/softwares.nix
@@ -13,8 +13,9 @@
     firefox
     ungoogled-chromium  # also you, require addtional arguments to work
 
-    # email
-    thunderbird
+    # messaging
+    thunderbird  # email
+    tdesktop  # tg
 
     # archive tools
     unzip
