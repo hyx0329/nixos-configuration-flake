3dd4c2a163c3d45bf95aad41383e191fe0827b9e
HuangYunXuan <hyx0329@163.com>
2022-08-26 15:00:03 +0800

    try to use web content for vim

--- home/hyx/vim/default.nix
+++ home/hyx/vim/default.nix
@@ -31,14 +31,13 @@
 
   # manual configuation
   home.file.".vimrc".source = ./vimrc;
-  home.file.".vim/autoload/plug.vim".source = ./plug.vim;
+  #home.file.".vim/autoload/plug.vim".source = ./plug.vim;
 
 
   # somehow the code below will not work after a garbage collection
-  #
-  #home.file.".vim/autoload/plug.vim".text = builtins.fetchurl {
-  #  # pure, but may not be the latest
-  #  url = "https://github.com/junegunn/vim-plug/raw/d94d234548a8fd6fa686812848f377f1419dafa1/plug.vim";
-  #  sha256 = "e2dbd7c8d732ae797e5059c0dc1e964b94529a32d07d051d5d058727462a43fd";
-  #};
+  home.file.".vim/autoload/plug.vim".source = builtins.fetchurl {
+    # pure, but may not be the latest
+    url = "https://github.com/junegunn/vim-plug/raw/d94d234548a8fd6fa686812848f377f1419dafa1/plug.vim";
+    sha256 = "e2dbd7c8d732ae797e5059c0dc1e964b94529a32d07d051d5d058727462a43fd";
+  };
 }
