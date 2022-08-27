f6117ba3d031fea8b22abd47c88674f02fba3ab5
HuangYunXuan <hyx0329@163.com>
2022-08-27 18:54:47 +0800

    fcitx: install single theme only

--- home/hyx/fcitx.nix
+++ home/hyx/fcitx.nix
@@ -5,13 +5,10 @@
     QT_IM_MODULE = "fcitx";  # for QT applications with fcitx
   };
 
-  home.file.".local/share/fcitx5/themes/Material-Color".source =
-    builtins.fetchTarball {
-      url = "https://github.com/hosxy/Fcitx5-Material-Color/archive/refs/tags/0.2.1.tar.gz";
-      # The sha256 value should be calculated through "nix-hash --type sha256 path/to/file"
-      # The sum is calculated AFTER the extraction.
-      # There could be a problem where the encoding of the sums are different.
-      sha256 = "8bd247209f9c1cb4c16543738fd5239772c87640a59535a93b54dae0e4f52d37";
+  home.file.".local/share/fcitx5/themes/Material-Color/theme.conf".source =
+    builtins.fetchurl {
+      url = "https://github.com/hosxy/Fcitx5-Material-Color/raw/2256feeae48dcc87f19a3cfe98f171862f8fcace/theme-teal.conf";
+      sha256 = "1ac4702dff31df82d75b9395c804e07e9df628d39d40d44bc82a2f89b5a5a48a";
     };
 
 }
