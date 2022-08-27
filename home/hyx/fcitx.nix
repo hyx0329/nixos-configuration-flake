0f0e65b5e09d37ae1de4b1aa35898c3a5abf60e0
HuangYunXuan <hyx0329@163.com>
2022-08-27 18:27:46 +0800

    generate hash use nix-hash

--- home/hyx/fcitx.nix
+++ home/hyx/fcitx.nix
@@ -8,7 +8,9 @@
   home.file.".local/share/fcitx5/themes/Material-Color".source =
     builtins.fetchTarball {
       url = "https://github.com/hosxy/Fcitx5-Material-Color/archive/refs/tags/0.2.1.tar.gz";
-      sha256 = "eeee748f47a645f2ba6f12258bd7c2a90016f842a8896511d88af42588d30594";
+      # The sha256 value should be calculated through "nix-hash --type sha256 path/to/file"
+      # There could be a problem where the encoding of the sums are different.
+      sha256 = "615d0f30c8ede30aacbe542dd6b09b6d308d50a79172380dfdf9ba1d1c6407c6";
     };
 
 }
