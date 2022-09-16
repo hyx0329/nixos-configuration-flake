0aca2f3a030391d825ef8034f597fa66c3f861e7
HuangYunXuan <hyx0329@163.com>
2022-09-16 15:25:21 +0800

    remove 32bit audio support

--- system/services/audio.nix
+++ system/services/audio.nix
@@ -8,7 +8,7 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
+    #alsa.support32Bit = true;
     pulse.enable = true;
     wireplumber.enable = true;
     # If you want to use JACK applications, uncomment this
