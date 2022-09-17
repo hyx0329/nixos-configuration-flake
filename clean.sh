8f66dee8cec1eea6c929f45b0727a98625088502
HuangYunXuan <hyx0329@163.com>
2022-09-17 14:19:36 +0800

    also clean home manager

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 nix-env --delete-generations old --profile /nix/var/nix/profiles/system
-nix-store --gc
+nix-env --delete-generations old --profile /nix/var/nix/profiles/per-user/*/home-manager
+
+if [ "$1" = "gc" ]; then
+  nix-store --gc
+fi
 
